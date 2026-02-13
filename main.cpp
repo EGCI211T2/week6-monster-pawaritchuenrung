b3d61ba87751313d2ff1487d86cb7cda73caf782
pawaritchuenrung <pawaritchuenrung@gmail.com>
2026-02-13 04:18:08 +0000

    week6

--- main.cpp
+++ main.cpp
@@ -8,19 +8,31 @@ int main(int argc, char* argv[]) {
 
     monster A("Siri ",10,1);
     monster B("Daisuke ");//name ,hp, potion if hp&potion are filled it will be set as default value
+    B+=10;
+    --A;
+
+    if(A>B) 
 
     monster x[5] = {{"James"},{"Michael"},{"Saharah"},{"Somchai"},{"Somying"}};
     monster C;
+    B.display();
+
+    /*
     monster *p;
     p=&C;
     p->display();
     p=&A
     p->display();
+
     p=new monster("Sarina",20);
     p->display();
     delete []p;//delete whenever u dont wanna use leaw
     p=newmonster[3];
     delete []p;
+    */   
+    p=new monster(50,2);
+
+    cout<<"======= The stronger hp is ===========\n"
     /*
  
  monster *p,x;
--- monster.h
+++ monster.h
@@ -8,18 +8,50 @@ private:
 public:
 	void Attack(monster &);
           void heal();
-	monster(string, int=1,int=1);
+	monster(string= "Anonymous", int=1,int=1);
+	
+	void monster::operator--(int a)
+	{
+		hp=-5;
+	} 
+	
+	void moster::operator+=(monster &x)
+	{
+		this->hp+=x.hp;
+        x.hp=0;
+	}
+	//ALT+126
 	~monster();
+	monster(int, int);
+	void operator+=(int);//Binary
+	void operator+=(int);//
+
+	void display();
+	bool operator>(monster &x);
 };
-monster :: monster(string n, int h, int p )
+bool monster::operator>(monster x){
+	if (hp>x.hp) return true;
+	else return false;
+}
+
+monster:: display()
+{
+	cout<<"Name   :"<<name <<endl;
+	cout<<"Hp     :"<<hp <<endl;
+	cout<<"Potion :"<<potion<<endl;
+
+
+}
+monster :: monster(string n, int h, int p )//default value
 {
 	name =n;
 	hp =h;
 	potion = p;
 	cout <<"Monster "<<name<<"is here"<< endl;
 	
-}
-monster:: ~monster()
+}//
+
+monster:: ~monster()//deconstructor
 {
 	cout<< name<<"is Deleted"<<endl;
 }
